d689cf1cdaa07133652fd65e3873540fabb84e66
olyapetryshyn <olya.petryshyn@gmail.com>
2019-10-29 09:50:23 +0200

    added Post model.

--- personal_blog/views.py
+++ personal_blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Corey',
-        'title': 'Blogpost1',
-        'content': 'bfjvbdjkv',
-        'date_posted': 'August 27, 2019'
-    },
-    {
-        'author': 'Olya',
-        'title': 'Blogpost2',
-        'content': 'bfjvbdjkvdfbdmbe',
-        'date_posted': 'August 28, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
