5bf8615d1b39b0c21fdb285c3a831aacad02ffaf
olyapetryshyn <olya.petryshyn@gmail.com>
2019-10-30 09:22:18 +0200

    added user registration.

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'personal_blog.apps.PersonalBlogConfig'
+    'personal_blog.apps.PersonalBlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
